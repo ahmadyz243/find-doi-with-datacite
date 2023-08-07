bd18d27d302d1d00f24ae9df76b17bb329153a3c
ahmad <ahmadyazdi243@gmail.com>
2023-08-07 23:35:17 +0330

    start project

---
+++ src/main/java/com/yazdi/finddoi/finddoi/FinddoiApplication.java
@@ -0,0 +1,13 @@
+package com.yazdi.finddoi.finddoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinddoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinddoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yazdi/finddoi/finddoi/FinddoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazdi.finddoi.finddoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinddoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
